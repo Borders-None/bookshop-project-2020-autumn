0e217619200f0a53b1fab61bd9c065813650cf73
Hany Afifi <hanyafifi@gmail.com>
2020-12-27 15:45:19 +0100

    adding the Author name and Book title to be shown in the browser address bar

--- app.js
+++ app.js
@@ -18,23 +18,34 @@ function getBooks() {
 function displayBooks(data) {
   let bookDiv = "";
   data.forEach((book) => {
-    const title = `<a class = 'title' href= "books/book.html?id=${book.id}">  ${book.title}</a>  `;
-
-    const author = `<div class = 'author'> ${book.author.first_name} ${book.author.last_name}</div> `;
-    const bookCover = `<img class = 'book-cover' src = ${book.cover_url} >`;
+    let authorFullName = book.author.first_name + " " + book.author.last_name;
+    let authorInUrl = authorFullName.replace(/\s+/g, "-");
+    let bookInUrl = book.title.replace(/\s+/g, "-").trim();
+    let title = `<a class = 'title' href= "books/book-details.html?id=${book.id}__${authorInUrl}__${bookInUrl}">${book.title}</a>  `;
+    let author = `<div class = 'author'> ${authorFullName}</div> `;
+    let bookCover = `<img class = 'book-cover' src = ${book.cover_url}>`;
     bookDiv += `<div class='book'> ${title} ${author} ${bookCover}   </div> `;
   });
   allBooks.innerHTML = bookDiv;
 }
 
+// function displayBooks(data) {
+//   let bookDiv = "";
+//   data.forEach((book) => {
+//     const title = `<a class = 'title' href= "books/book.html?id=${book.id}">  ${book.title}</a>  `;
+//     const author = `<div class = 'author'> ${book.author.first_name} ${book.author.last_name}</div> `;
+//     const bookCover = `<img class = 'book-cover' src = ${book.cover_url} >`;
+//     bookDiv += `<div class='book'> ${title} ${author} ${bookCover}   </div> `;
+//   });
+//   allBooks.innerHTML = bookDiv;
+// }
+
 // Login Form popup
 document.getElementById("btn-Form").addEventListener("click", openUserForm);
 document.getElementById("btn-close").addEventListener("click", closeUserForm);
-
 function openUserForm() {
   document.getElementById("userForm").style.display = "block";
 }
-
 function closeUserForm() {
   document.getElementById("userForm").style.display = "none";
 }
